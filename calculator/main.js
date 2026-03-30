095636c7de7c473c5cb2ac3726323436679ec686
okolodk <grisha.zhivot@bk.ru>
2026-03-30 17:44:41 +0300

    Step 5: refactoring code for calculator

--- calculator/main.js
+++ calculator/main.js
@@ -1,26 +1,7 @@
-const display = document.getElementById("display");
-const buttons = document.querySelectorAll("button");
-const clearBtn = document.getElementById("clear");
-let expression = "";
-buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-        const value = button.textContent;
-        if (value === "C"){
-            expression = "";
-            display.value = "";
-            return;
-        }
-        if (value === "="){
-            try{
-                expression = eval(expression);
-                display.value = expression;   
-            } catch{
-                display.value = "Ошибка";
-                expression = "";
-            }
-            return;
-        }
-        expression += value;
-        display.value = expression;
-    });
+import { Calculator } from './calculator.js';
+document.addEventListener('DOMContentLoaded', () => {
+    const display = document.getElementById('display');
+    const buttons = document.querySelectorAll('button');
+    const calc = new Calculator(display);
+    calc.init(buttons);
 });
